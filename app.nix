2482e41bcf9c63bbb459e9a427b95ec53c02a5a4
IvanPostu <ipostu20000127@gmail.com>
2026-05-28 19:05:55 +0300

    init: setup project

---
+++ app.nix
@@ -0,0 +1,24 @@
+let 
+  pkgs = import <nixpkgs> { config = { allowUnfree = false; }; };
+  PROJECT_ROOT = builtins.toString ./.;
+in
+pkgs.mkShell {
+  name = "app-shell";
+
+  buildInputs = [
+    pkgs.jdk21
+  ];
+
+  LANG = "en_US.UTF-8";
+  LC_ALL = "en_US.UTF-8";
+
+  shellHook = ''
+    export PROJECT_ROOT=${PROJECT_ROOT}
+
+    export JAVA_HOME=${pkgs.jdk21}
+    export PATH=${pkgs.jdk21}/bin:$PATH
+
+    chmod -R +x ${PROJECT_ROOT}/scripts
+    export PATH=${PROJECT_ROOT}/scripts:$PATH
+  '';
+}
---
+++ rabbitmq-demo-parent/program1/src/main/java/com/ipostu/rabbitmq/demo/program1/App.java
@@ -0,0 +1,8 @@
+package com.ipostu.rabbitmq.demo.program1;
+
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
