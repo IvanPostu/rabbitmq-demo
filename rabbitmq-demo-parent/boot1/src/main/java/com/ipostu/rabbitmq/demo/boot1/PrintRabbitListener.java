ec843889605cd5704ca38c905d19c4495c5849dd
IvanPostu <ipostu20000127@gmail.com>
2026-06-01 22:12:05 +0300

    chore: rabbitmq consume message in binary

--- rabbitmq-demo-parent/boot1/src/main/java/com/ipostu/rabbitmq/demo/boot1/PrintRabbitListener.java
+++ rabbitmq-demo-parent/boot1/src/main/java/com/ipostu/rabbitmq/demo/boot1/PrintRabbitListener.java
@@ -5,16 +5,37 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
 @Slf4j
 @Component
 public class PrintRabbitListener {
 
 //    @RabbitListener(queues = {"Mobile"})
-    public void consume(Person person, Message message) {
+//    public void consume(Person person, Message message) {
+//        log.info("Consumed: {}", person);
+//
+//        var props = message.getMessageProperties();
+//
+//        log.info("Headers: {}", props.getHeaders());
+//        log.info("Exchange: {}", props.getReceivedExchange());
+//        log.info("Routing Key: {}", props.getReceivedRoutingKey());
+//        log.info("Delivery Tag: {}", props.getDeliveryTag());
+//    }
+
+    @RabbitListener(queues = {"Mobile"})
+    public void consume(byte[] payload, Message message) throws IOException, ClassNotFoundException {
+
+        ByteArrayInputStream bis = new ByteArrayInputStream(payload);
+        ObjectInput in = new ObjectInputStream(bis);
+        Person person = (Person) in.readObject();
+
         log.info("Consumed: {}", person);
 
         var props = message.getMessageProperties();
-
         log.info("Headers: {}", props.getHeaders());
         log.info("Exchange: {}", props.getReceivedExchange());
         log.info("Routing Key: {}", props.getReceivedRoutingKey());
